04e31357bb9d13b634f04975158a5274f993afe1
mplanelles <mplanelles@cxignited.com>
2018-10-05 22:43:29 +0200

    Changed theme to kaolin-galaxy

--- visual.el
+++ visual.el
@@ -1,3 +1,8 @@
-(use-package smyx-theme
-	     :config
-	     (load-theme 'smyx))
+;; (use-package smyx-theme
+;;   :config
+;;   (load-theme 'smyx))
+
+(use-package kaolin-themes
+  :config
+  (load-theme 'kaolin-galaxy))
+  
